450abd383452c5d4505e7e15b6932b91c2b19252
tonidev97 <antonio.kamber@brainit.agency>
2022-02-08 06:35:06 +0100

    Add fakeFlag chain, not in the build

--- components/TestDiv.tsx
+++ components/TestDiv.tsx
@@ -2,9 +2,11 @@ import getConfig from "next/config"
 
 export const TestDiv = () => {
   const { publicRuntimeConfig } = getConfig()
-  const fakeFlag = false
+  const fakeFlag = false,
+    fakeFlag1 = true,
+    fakeFlag2 = true
 
-  return fakeFlag ? <div>Div feature flag test</div> : null
+  return fakeFlag1 && fakeFlag2 && fakeFlag ? <div>Div feature flag test</div> : null
 }
 
 export default TestDiv
