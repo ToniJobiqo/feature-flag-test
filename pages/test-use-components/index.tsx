13b088ded11e51b1503442c3ba9f60f425325153
tonidev97 <antonio.kamber@brainit.agency>
2022-02-08 10:45:30 +0100

    Returning component value, two times in the build

--- pages/test-use-components/index.tsx
+++ pages/test-use-components/index.tsx
@@ -1,10 +1,15 @@
 import TestUseComponents from "components/TestUseComponents"
+import { NextPageContext } from "next"
 import React from "react"
 
 interface Props {
   flagTest: boolean
 }
 export default class TestUseComponentsPage extends React.Component<Props> {
+  static async getInitialProps({ req }: NextPageContext) {
+    return { flagTest: false }
+  }
+
   render() {
     const { flagTest } = this.props
     console.log("flagTest:", flagTest)
